cb29de6b3db7a21c86b187339af5a49660b9a537
usaidnova <usaid.ather@wearenova.co.uk>
2022-08-20 18:06:27 +0500

    Initial Commit

---
+++ TrinityWizardsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrinityWizardsTest
+//
+//  Created by Usaid Ather on 20/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
